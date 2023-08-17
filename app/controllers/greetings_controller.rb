bb31c02032ea6e80f8276ce1be7dcac60cce68e5
Sasan Moshirabadi <sasan.dev89@gmail.com>
2023-08-18 01:14:19 +0300

    Add linter configs + Fix linter errors

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -2,8 +2,8 @@ class GreetingsController < ApplicationController
   def index
     @greetings_json = Greeting.find(rand(1..5))
     p @greetings_json
-    if request.content_type == 'application/json'
-      render json: @greetings_json
-    end
+    return unless request.content_type == 'application/json'
+
+    render json: @greetings_json
   end
 end
